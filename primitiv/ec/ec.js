dbe163b4caf5ca9bdfed6066e5ca29f1126cdbbf
Madoka-Zhang <1263960582@qq.com>
2023-07-12 12:45:11 +0800

    Elgamal is correct

--- primitiv/ec/ec.js
+++ primitiv/ec/ec.js
@@ -1,13 +1,13 @@
-const EC = require('elliptic').ec;
-const ec = new EC('secp256k1');
-
+var EC = require('elliptic').ec;
+var ec = new EC('secp256k1');
+var BN = require('bn.js');
 
 EC.prototype.randomBN = function() {
-
+    return new BN(1234);
 };
 
 EC.prototype.randomPoint = function() {
-    return this.curve.g.mul(randomBN);
+    return this.curve.g.mul(this.randomBN());
 };
 
 module.exports = ec;
--- primitiv/encryption/ElgamalEncryption.js
+++ primitiv/encryption/ElgamalEncryption.js
@@ -1,83 +1,110 @@
-var ElgamalCiphertext = require("./ElgamalCiphertext");
 var BN = require('bn.js');
 var randomBytes = require('randombytes');
 
-function ElgamalEnc() {
-  ElgamalEnc.encrypt = function(pubKey, randomness, msg) {
-    g_r = pubKey.curve.g.mul(randomness);
-    pk_r = randomness.isZero() ? pubKey.curve.g : pubKey.mul(r);
-    return ElgamalCiphertext(g_r, pk_r.add(msg));
-  }
-
-  ElgamalEnc.decrypt = function(privKey, ciphertext) {
-    c1_privKey = ciphertext.c1.mul(privKey);
-    return ciphertext.c2.add(c1_privKey.neg());
-  }
-
-  ElgamalEnc.test = function() {
-    var EC = require('../ec/ec');
-    // var ec = new EC('sepc256k1');
-
-    var key = ec.genKeyPair();
-
-    var randomness = ec.randomBN();
-    var msg = ec.randomPoint();
-
-    console.log(msg == decrypt(key.privKey, encrypt(key.pubKey, randomness, msg)));
-  }
+function ElgamalCiphertext(c1, c2) {
+  this.c1 = c1;
+  this.c2 = c2;
 }
 
+ElgamalCiphertext.prototype.mul = function(e) {
+  return ElgamalCiphertext(this.c1.mul(e), this.c2.mul(e));
+}
+
+ElgamalCiphertext.prototype.add = function(other) {
+  return ElgamalCiphertext(this.c1.add(other.c1), this.c2.add(other.c2));
+}
+
+ElgamalCiphertext.prototype.neg = function() {
+  return ElgamalCiphertext(this.c1.neg(), this.c2.neg());
+}
+
+function ElgamalEnc() {
+  
+}
+
+ElgamalEnc.encrypt = function(pubKey, randomness, msg, curve) {
+  g_r = curve.g.mul(randomness);
+  pk_r = randomness.isZero() ? curve.g : pubKey.mul(randomness);
+  // console.log(pk_r, curve.g);
+  return new ElgamalCiphertext(g_r, pk_r.add(msg));
+}
+
+ElgamalEnc.decrypt = function(privKey, ciphertext, curve) {
+  c1_privKey = ciphertext.c1.mul(privKey);
+  return ciphertext.c2.add(c1_privKey.neg());
+}
+
+ElgamalEnc.test = function() {
+  var ec = require('../ec/ec');
+  // var ec = new EC('sepc256k1');
+
+  var key = ec.genKeyPair();
+  var pubKey = key.getPublic();
+  var privKey = key.getPrivate();
+  // console.log(key.pubKey);
+
+  var randomness = ec.randomBN();
+  var msg = ec.randomPoint();
+
+  console.log(msg.eq(this.decrypt(privKey, this.encrypt(pubKey, randomness, msg, ec.curve), ec.curve)));
+}
+
+
+// ElgamalEnc.test();
 
 function LiftedElgamalEnc() {
-  /**
-   * @const
-   */
-  var MAX_EXP = new BN(1048576);
   
-  LiftedElgamalEnc.encryptWithRandomness = function(pubKey, randomness, msg) {
-    g_msg = pubKey.curve.g.mul(msg);
-    return ElgamalEnc.encrypt(pubKey, randomness, g_msg);
-  }
+}
 
-  LiftedElgamalEnc.encrypt = function(pubKey, msg) {
-    var randomBytesArray = randomBytes(16);
-    var randomness = new BN(randomBytesArray).mod(MAX_EXP);
-    return [encryptWithRandomness(pubKey, randomness, msg), randomness];
-  }
+LiftedElgamalEnc.encryptWithRandomness = function(pubKey, randomness, msg, curve) {
+  g_msg = curve.g.mul(msg);
+  return ElgamalEnc.encrypt(pubKey, randomness, g_msg, curve);
+}
 
-  LiftedElgamalEnc.decrypt = function(privKey, ciphertext) {
-    g_msg = ElgamalEnc.decrypt(privKey, ciphertext);
-    return dlog(g_msg, privKey.curve);
-  }
+LiftedElgamalEnc.encrypt = function(pubKey, msg, curve) {
+  var randomBuffer = randomBytes(20);
+  var randomBN = new BN(randomBuffer);
+  this.MAX_EXP = new BN(1048576);
+  var randomness = randomBN.mod(this.MAX_EXP);
+  return [this.encryptWithRandomness(pubKey, randomness, msg, curve), randomness];
+}
 
-  LiftedElgamalEnc.dlog = function(value, curve) {
-    if (curve.isInfinity(value)) {
-      return new BN(0);
+LiftedElgamalEnc.decrypt = function(privKey, ciphertext, curve) {
+  g_msg = ElgamalEnc.decrypt(privKey, ciphertext);
+  return this.dlog(g_msg, curve);
+}
+
+LiftedElgamalEnc.dlog = function(value, curve) {
+  if (value.isInfinity()) {
+    return new BN(0);
+  } else {
+    var exp = new BN(1);
+
+    while (exp.lte(this.MAX_EXP) && !(curve.g.mul(exp).eq(value))) {
+      exp = exp.add(new BN(1));
+    }
+
+    if (exp.gt(this.MAX_EXP)) {
+      return null;
     } else {
-      var exp = new BN(1);
-
-      while (exp.lte(MAX_EXP) && !(curve.g.mul(exp) == value)) {
-        exp = exp.add(new BN(1));
-      }
-
-      if (exp.gt(MAX_EXP)) {
-        return null;
-      } else {
-        return exp;
-      }
+      return exp;
     }
   }
-
-  LiftedElgamalEnc.test = function() {
-    
-    var EC = require('../ec/ec');
-    // var ec = new EC('sepc256k1');
-
-    var key = ec.genKeyPair();
-
-    var randomness = ec.randomBN();
-    var msg = ec.randomBN();
-
-    console.log(msg == decrypt(key.privKey, encrypt(key.pubKey, msg)));
-  }
 }
+
+LiftedElgamalEnc.test = function() {
+  
+  var ec = require('../ec/ec');
+  // var ec = new EC('sepc256k1');
+
+  var key = ec.genKeyPair();
+  var pubKey = key.getPublic();
+  var privKey = key.getPrivate();
+
+  var BN1024 = new BN(1024);
+  var msg = ec.randomBN().mod(BN1024);
+
+  console.log(msg.eq(this.decrypt(privKey, this.encrypt(pubKey, msg, ec.curve)[0], ec.curve)));
+}
+
+// LiftedElgamalEnc.test();
