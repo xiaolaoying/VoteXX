567f99f804fa27089d06d0d4fa7fd1275b3482f3
Zhang Xiaoyuan <72085233+Madoka-Zhang@users.noreply.github.com>
2023-07-24 22:10:46 +0800

    Crypto: Nullification NIZK ready (#8)
    * init elgamal
    
    * added test, not yet tested
    
    * delete sth
    
    * Elgamal is correct
    
    * fix random
    
    * add elgamal example
    
    * add elgamal example
    
    * fix random
    
    * fix random
    
    * fix errors
    
    * init struct
    
    * add some function
    
    * finish private
    
    * add other functions
    
    * add code
    
    * run, but false
    
    * fix condition 1-3
    
    * fix sth
    
    * fix condition&& verify
    
    * rebuild Nullification
    
    * fix sim
    
    * add time benchmark
    
    * add benchmark
    
    * fix speed
    
    * merge and fix
    
    * add readme
    
    * del __pycache__

--- primitiv/ec/ec.js
+++ primitiv/ec/ec.js
@@ -12,4 +12,25 @@ EC.prototype.randomPoint = function() {
     return this.curve.g.mul(this.randomBN());
 };
 
+EC.prototype.serializedPoint = function(point) {
+    return point.encode("hex", true);
+}
+
+EC.prototype.vecOfPointsToBytes = function(points) {
+    return points.map(point => point.encode('hex', true));
+}
+
+EC.prototype.deserializedPoint = function(serializedPoint) {
+    var point = ec.curve.decodePoint(serializedPoint, 'hex');
+    return point;
+}
+
+EC.prototype.vecOfPointsFromBytes = function(bytes) {
+    return bytes.map(str => ec.curve.decodePoint(str, 'hex'));
+}
+
+// EC.prototype.pointByteSize = function(point) {
+//     return point.x.byteLength()+2;
+// }
+
 module.exports = ec;
