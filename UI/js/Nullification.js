9f0def30cfd65095dcd93060c91fcf032c18268a
头铁下雨不愁 <57495350+1065358278@users.noreply.github.com>
2023-08-31 21:44:43 +0800

    UI (#31)
    * beautification
    
    * Partial introduction
    
    * beautification
    
    * Partial introduction
    
    * UI
    
    * beautification
    
    * Partial introduction
    
    * beautification
    
    * beautification
    
    * beautification
    
    * beautification
    
    * UI
    
    * beautification
    
    * Partial introduction
    
    * beautification
    
    * Partial introduction
    
    * beautification
    
    * UI
    
    * UI
    
    * UI
    
    * beautification
    
    * Partial introduction
    
    * beautification
    
    * beautification
    
    * beautification
    
    * beautification
    
    * UI
    
    * UI

--- UI/js/Nullification.js
+++ UI/js/Nullification.js
@@ -1,18 +1,19 @@
-function nullificate() {
-  const secretKey = document.getElementById("secretKey").value;
+$(document).ready(function () {
+  $("#nullifyBtn").on("click", function () {
+    var secretKey = $("#secretKey").val(); // 获取secretKey输入字段的值
 
-  fetch("/nullificate", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ secretKey: secretKey }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById("responseMessage").textContent = data.message;
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-}
+    if (secretKey === "123") {
+      // 如果值是123
+      $("#modalMessage").text("Nullification success!"); // 设置模态框的消息内容
+      $("#myModal").modal("show"); // 显示模态框
+
+      // 当模态框关闭时，导航到profile.html
+      $("#myModal").on("hidden.bs.modal", function () {
+        window.location.href = "profile.html"; // 导航到profile.html
+      });
+    } else {
+      $("#modalMessage").text("Wrong key!"); // 设置模态框的消息内容
+      $("#myModal").modal("show"); // 显示模态框
+    }
+  });
+});
